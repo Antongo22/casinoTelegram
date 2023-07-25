531a2a9cb08dc741e48f0e4766edc8e3d406d78e
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-07-26 00:34:00 +0300

    есть анкета

--- casinoTelegram/Program.cs
+++ casinoTelegram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
@@ -13,12 +14,23 @@ namespace casinoTelegram
             Default,
             ChooseRange,
             Game,
-            GameUpTo10
+            GameUpTo10,
+            MySurvey
         }
 
         private static BotState currentState = BotState.Default;
         private static int targetNumber;
         private static int maxNumber;
+        private static Dictionary<long, UserData> userDataDict = new Dictionary<long, UserData>();
+
+        private class UserData
+        {
+            public string FirstName;
+            public string LastName;
+            public int Age;
+            public int LuckyNumber;
+            public int State; // Добавлено поле State
+        }
 
         static void Main(string[] args)
         {
@@ -26,6 +38,7 @@ namespace casinoTelegram
             client.StartReceiving(Update, Error);
             Console.WriteLine("Бот запущен. Нажмите любую клавишу, чтобы остановить.");
             Console.ReadKey();
+ 
         }
 
         async static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
@@ -50,6 +63,9 @@ namespace casinoTelegram
                     case BotState.GameUpTo10:
                         await HandleGameUpTo10State(client, message);
                         break;
+                    case BotState.MySurvey:
+                        await HandleMySurveyState(client, message);
+                        break;
                 }
             }
         }
@@ -59,14 +75,18 @@ namespace casinoTelegram
             switch (message.Text)
             {
                 case "/start":
-                    await client.SendTextMessageAsync(message.Chat.Id, "Привет! Добро пожаловать в наше казино! Введите /play, чтобы начать игру.");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Привет! Добро пожаловать в наше казино! Введите /play, чтобы начать игру, или /survey, чтобы заполнить анкету.");
                     break;
                 case "/play":
                     await client.SendTextMessageAsync(message.Chat.Id, "Выберите диапазон чисел:\n1. От 1 до 10\n2. От 1 до 100");
                     currentState = BotState.ChooseRange;
                     break;
+                case "/survey":
+                    await client.SendTextMessageAsync(message.Chat.Id, "Давайте заполним вашу анкету. Введите ваше имя:");
+                    currentState = BotState.MySurvey;
+                    break;
                 default:
-                    await client.SendTextMessageAsync(message.Chat.Id, "Я не понимаю вашей команды. Введите /play для игры.");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Я не понимаю вашей команды. Введите /start для начала, /play для игры, или /survey для заполнения анкеты.");
                     break;
             }
         }
@@ -134,6 +154,44 @@ namespace casinoTelegram
             else
             {
                 await client.SendTextMessageAsync(message.Chat.Id, "Пожалуйста, введите только число.");
+            }
+        }
+
+        async static Task HandleMySurveyState(ITelegramBotClient client, Message message)
+        {
+            long chatId = message.Chat.Id;
+            if (!userDataDict.ContainsKey(chatId))
+            {
+                userDataDict[chatId] = new UserData();
+                userDataDict[chatId].State = 0;
+            }
+
+            switch (userDataDict[chatId].State)
+            {
+                case 0:
+                    userDataDict[chatId].FirstName = message.Text;
+                    await client.SendTextMessageAsync(message.Chat.Id, "Отлично! Теперь введите вашу фамилию:");
+                    userDataDict[chatId].State = 1;
+                    break;
+                case 1:
+                    userDataDict[chatId].LastName = message.Text;
+                    await client.SendTextMessageAsync(message.Chat.Id, "Отлично! Теперь введите ваш возраст:");
+                    userDataDict[chatId].State = 2;
+                    break;
+                case 2:
+                    if (int.TryParse(message.Text, out int age))
+                    {
+                        userDataDict[chatId].Age = age;
+                        userDataDict[chatId].LuckyNumber = new Random().Next(1, 101);
+                        await client.SendTextMessageAsync(message.Chat.Id, "Отлично! Ваши данные сохранены.");
+                        await client.SendTextMessageAsync(message.Chat.Id, $"Ваше имя: {userDataDict[chatId].FirstName}\nФамилия: {userDataDict[chatId].LastName}\nВозраст: {userDataDict[chatId].Age}\nВаше счастливое число: {userDataDict[chatId].LuckyNumber}");
+                        currentState = BotState.Default;
+                    }
+                    else
+                    {
+                        await client.SendTextMessageAsync(message.Chat.Id, "Пожалуйста, введите ваш возраст числом.");
+                    }
+                    break;
             }
         }
 
