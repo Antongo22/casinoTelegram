600e73744f949378e1a750b6d10b892a7845484a
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-07-25 20:52:34 +0300

    база есть!

--- casinoTelegram/Program.cs
+++ casinoTelegram/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace casinoTelegram
 {
@@ -10,6 +13,24 @@ namespace casinoTelegram
     {
         static void Main(string[] args)
         {
+            var client = new TelegramBotClient("6254402236:AAF-lAzwr4E1XjicyVw_Y6ENLNsilvAZwJM");
+            client.StartReceiving(Update, Error);
+            Console.ReadKey();
+        }
+
+        async static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if(message.Text != null)
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, $"Ты написал - {message.Text}");
+                return;
+            }
+        }
+
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
         }
     }
 }
