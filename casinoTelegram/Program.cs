21f2d71e91313f000567273915e0a339067474e5
Antongo22 <80984465+Antongo22@users.noreply.github.com>
2023-07-26 00:20:16 +0300

    продвинутая игра

--- casinoTelegram/Program.cs
+++ casinoTelegram/Program.cs
@@ -11,10 +11,14 @@ namespace casinoTelegram
         private enum BotState
         {
             Default,
-            Game
+            ChooseRange,
+            Game,
+            GameUpTo10
         }
 
         private static BotState currentState = BotState.Default;
+        private static int targetNumber;
+        private static int maxNumber;
 
         static void Main(string[] args)
         {
@@ -22,14 +26,13 @@ namespace casinoTelegram
             client.StartReceiving(Update, Error);
             Console.WriteLine("Бот запущен. Нажмите любую клавишу, чтобы остановить.");
             Console.ReadKey();
-
         }
 
         async static Task Update(ITelegramBotClient client, Update update, CancellationToken token)
         {
             var message = update.Message;
 
-            Console.WriteLine($"{message.Chat.FirstName ?? "--no name--"}\t\t|\t{message.Text ?? "--no text--"}");
+            Console.WriteLine($"{message.Chat.FirstName ?? "-no name-"}\t\t|\t{message.Text ?? "-no text-"}");
 
             if (message.Text != null)
             {
@@ -38,8 +41,14 @@ namespace casinoTelegram
                     case BotState.Default:
                         await HandleDefaultState(client, message);
                         break;
+                    case BotState.ChooseRange:
+                        await HandleChooseRangeState(client, message);
+                        break;
                     case BotState.Game:
                         await HandleGameState(client, message);
+                        break;
+                    case BotState.GameUpTo10:
+                        await HandleGameUpTo10State(client, message);
                         break;
                 }
             }
@@ -53,11 +62,33 @@ namespace casinoTelegram
                     await client.SendTextMessageAsync(message.Chat.Id, "Привет! Добро пожаловать в наше казино! Введите /play, чтобы начать игру.");
                     break;
                 case "/play":
-                    await client.SendTextMessageAsync(message.Chat.Id, "Давайте начнем игру! Отгадайте число от 1 до 10. Введите число:");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Выберите диапазон чисел:\n1. От 1 до 10\n2. От 1 до 100");
+                    currentState = BotState.ChooseRange;
+                    break;
+                default:
+                    await client.SendTextMessageAsync(message.Chat.Id, "Я не понимаю вашей команды. Введите /start для начала или /play для игры.");
+                    break;
+            }
+        }
+
+        async static Task HandleChooseRangeState(ITelegramBotClient client, Message message)
+        {
+            switch (message.Text)
+            {
+                case "1":
+                    maxNumber = 10;
+                    targetNumber = new Random().Next(1, maxNumber + 1);
+                    await client.SendTextMessageAsync(message.Chat.Id, $"Вы выбрали диапазон от 1 до {maxNumber}. Давайте начнем игру! Отгадайте число от 1 до {maxNumber}. Введите число:");
+                    currentState = BotState.GameUpTo10;
+                    break;
+                case "2":
+                    maxNumber = 100;
+                    targetNumber = new Random().Next(1, maxNumber + 1);
+                    await client.SendTextMessageAsync(message.Chat.Id, $"Вы выбрали диапазон от 1 до {maxNumber}. Давайте начнем игру! Отгадайте число от 1 до {maxNumber}. Введите число:");
                     currentState = BotState.Game;
                     break;
                 default:
-                    await client.SendTextMessageAsync(message.Chat.Id, "Я не понимаю вашей команды. Введите /play для игры.");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Пожалуйста, выберите 1 или 2 для выбора диапазона.");
                     break;
             }
         }
@@ -66,22 +97,43 @@ namespace casinoTelegram
         {
             if (int.TryParse(message.Text, out int guessedNumber))
             {
-                // Произвольное число от 1 до 10
-                Random random = new Random();
-                int targetNumber = random.Next(1, 11);
-
                 if (guessedNumber == targetNumber)
                 {
-                    await client.SendTextMessageAsync(message.Chat.Id, "Поздравляю! Вы угадали число! Введите /play, чтобы сыграть еще раз.");
+                    await client.SendTextMessageAsync(message.Chat.Id, "Поздравляю! Вы угадали число!");
+                    currentState = BotState.Default;
+                }
+                else if (guessedNumber < targetNumber)
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, $"Нет, загаданное число больше {guessedNumber}. Попробуйте еще раз! Введите число:");
                 }
                 else
                 {
-                    await client.SendTextMessageAsync(message.Chat.Id, $"Увы, вы не угадали. Загаданное число было: {targetNumber}. Попробуйте еще раз! Введите число:");
+                    await client.SendTextMessageAsync(message.Chat.Id, $"Нет, загаданное число меньше {guessedNumber}. Попробуйте еще раз! Введите число:");
                 }
             }
             else
             {
-                await client.SendTextMessageAsync(message.Chat.Id, "Пожалуйста, введите только число от 1 до 10.");
+                await client.SendTextMessageAsync(message.Chat.Id, "Пожалуйста, введите только число.");
+            }
+        }
+
+        async static Task HandleGameUpTo10State(ITelegramBotClient client, Message message)
+        {
+            if (int.TryParse(message.Text, out int guessedNumber))
+            {
+                if (guessedNumber == targetNumber)
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, "Поздравляю! Вы угадали число!");
+                    currentState = BotState.Default;
+                }
+                else
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, "Увы, вы не угадали. Попробуйте еще раз! Введите число:");
+                }
+            }
+            else
+            {
+                await client.SendTextMessageAsync(message.Chat.Id, "Пожалуйста, введите только число.");
             }
         }
 
